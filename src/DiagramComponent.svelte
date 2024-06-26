5ec7f158411effd79d49415461fbbca67c5d27ec
Ivan Izobov <izobov9@gmail.com>
2024-06-26 12:36:46 +0300

    dev: gh pages

--- src/DiagramComponent.svelte
+++ src/DiagramComponent.svelte
@@ -1,22 +1,24 @@
 <script>
-    import { onMount } from "svelte";
-    
-    import "@dhx/trial-diagram/codebase/diagram.min.css";
-    import { DiagramEditor } from "@dhx/trial-diagram";
-    
-    export let data;
+  import { onMount } from "svelte";
 
-    let diagram;
-    let container;
-    onMount(() => {
-        diagram = new DiagramEditor(container, {type: "default"  });
-        
-        return () => {
-            diagram.destructor();
-        };
+  import "@dhx/trial-diagram/codebase/diagram.min.css";
+  import { DiagramEditor } from "@dhx/trial-diagram";
+
+  export let data;
+
+  let diagram;
+  let container;
+  onMount(() => {
+    diagram = new DiagramEditor(container, {
+      type: "default",
+      shapeToolbar: true,
     });
-    $: diagram?.parse(data);
+
+    return () => {
+      diagram.destructor();
+    };
+  });
+  $: diagram?.parse(data);
 </script>
 
 <div bind:this={container} style="width: 100%; height: 100%;"></div>
-
